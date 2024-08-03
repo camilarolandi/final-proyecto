cb32f2ecf3b2ee47a38935d3760e8786254c21eb
camilarolandi <rolandi.camil@gmail.com>
2024-08-03 18:10:19 -0300

    fix

--- src/Components/Mensajes/Mensajes.jsx
+++ src/Components/Mensajes/Mensajes.jsx
@@ -1,5 +1,5 @@
 /* Cada globito de mensaje*/
-import React from 'react'
+/* import React from 'react'
 import "./mensajes.css"
 import '../../styles.css'
 
@@ -21,5 +21,32 @@ export const Mensajes = ({ mensaje }) => {
                 </div>
             </div>
         </>
+    )
+}
+ */
+import React from 'react'
+import "./mensajes.css"
+import '../../styles.css'
+
+export const Mensajes = ({ mensaje }) => {
+    const { author, text, id, day, hour, estado } = mensaje
+
+    return (
+        <div className={`contenedor-chat ${author === "Yo" && "derecha"}`} key={id}>
+            <div className={`chats ${author === "Yo" && "der"}`}>
+                <span className='author'>{author}</span>
+                <p className='mensaje-texto'>{text}</p>
+                {author === "Yo" && (
+                    <div className='contenedor-inferior'>
+                        <span className='day-hour'>{`${day} ${hour}`}</span>
+                        <div className='estado-cont'>
+                            <span>{estado}</span>
+                            {estado === "Visto" && <i className="bi bi-check2-all"></i>}
+                            {estado === "Entregado" && <i className="bi bi-check2"></i>}
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
     )
 }
